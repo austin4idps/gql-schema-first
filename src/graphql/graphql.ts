cff59d896e7571cdf896f48815653106cbd7e88d
austin4idps <austin@4idps.com>
2023-06-08 13:52:26 +0800

    chore(gql): init graphql render file

---
+++ src/graphql/graphql.ts
@@ -0,0 +1,56 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export enum MemberTypeEnum {
+    Admin = "Admin",
+    User = "User",
+    Vip = "Vip"
+}
+
+export class ProfileInput {
+    email?: Nullable<string>;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+}
+
+export class CreateUserInput {
+    displayName: string;
+    memberType: MemberTypeEnum;
+    profile?: Nullable<ProfileInput>;
+}
+
+export class MemberPofile {
+    id: string;
+    memberId: string;
+    email?: Nullable<string>;
+    firstName?: Nullable<string>;
+    lastName?: Nullable<string>;
+}
+
+export class Member {
+    id: string;
+    displayName?: Nullable<string>;
+    memberType?: Nullable<MemberTypeEnum>;
+    profile?: Nullable<MemberPofile>;
+}
+
+export abstract class IQuery {
+    abstract getUser(id: string): Nullable<Member> | Promise<Nullable<Member>>;
+}
+
+export class CreateUserRes {
+    memberId?: Nullable<string>;
+}
+
+export abstract class IMutation {
+    abstract createUser(input?: Nullable<CreateUserInput>): Nullable<CreateUserRes> | Promise<Nullable<CreateUserRes>>;
+}
+
+type Nullable<T> = T | null;
